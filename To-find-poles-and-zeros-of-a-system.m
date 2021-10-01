f7160fb3922d3f977da4fdec2e0073a2c1923cf2
Sajjad Ullah <91423397+SU008@users.noreply.github.com>
2021-10-01 15:54:45 +0100

    Add files via upload

---
+++ To-find-poles-and-zeros-of-a-system.m
@@ -0,0 +1,12 @@
+
+
+
+%To find the poles and zeros.  
+
+
+% a,b values taken from a Signal Flow Diagram or a Difference Equation, refer to word document for more info. 
+b = [1 0.68346 0.64]
+a = [1 0.54098 0.09]
+zeros = roots(b)
+poles = roots(a)
+
